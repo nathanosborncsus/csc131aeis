f7b1eea89ad1071ec4c9965dd245f88eb72f501f
Nathan O <nathanosborn@csus.edu>
2018-05-10 23:35:49 -0700

    Mike's changes for reading from the database for the dashboard

--- phpproj/phpproj/dashboard.php
+++ phpproj/phpproj/dashboard.php
@@ -23,41 +23,60 @@
      ?>
 
 
-
-
 <table>
 
   <tr>
-    <th>Date Submitted</th>
     <th>Assessment Name</th>
     <th>Total Score</th>
+    <th>Date Submitted</th>
     <th>Action</th>
   </tr>
 
-  <tr>
-    <td>01/05/2018 09:31AM</td>
-    <td>System 1</td>
-    <td>54</td>
-    <td> VIEW | DELETE</td>
-  </tr>
+  <?php
 
-  <tr>
-    <td>03/11/2018 04:21PM</td>
-    <td>System 2</td>
-    <td>70</td>
-    <td> VIEW | DELETE</td>
-  </tr>
+    $uid = $_SESSION["u_id"];
 
-  <tr>
-    <td>04/15/2018 11:11AM</td>
-    <td>System 3</td>
-    <td>44</td>
-    <td> VIEW | DELETE</td>
-  </tr>
+    $sql = "SELECT * FROM assessments WHERE report_owner='$uid'";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    $count = 0;
+
+    if($resultCheck > 0) {
+      while (($row = mysqli_fetch_assoc($result)) && ($count < 10)) {
+        if ($row['active'] > 0) {
+          echo "<tr>";
+          echo "<th>" . $row['report_name'] . "</th>";
+
+          $score = 0;
+          $question1 = $row['question_1'];
+          $question2 = $row['question_2'];
+          $question3 = $row['question_3'];
+          $question4 = $row['question_4'];
+          $question5 = $row['question_5'];
+          $question6 = $row['question_6'];
+          $question7 = $row['question_7'];
+          $question8 = $row['question_8'];
+          $question9 = $row['question_9'];
+          $question10 = $row['question_10'];
 
 
 
+          echo "<th> 100 </th>";
+          echo "<th>" . $row['dt_created'] . "</th>";
+          echo "<th> Delete </th>"; //this need to be a link that will send a request to move 'active' to 0
 
+          echo "</tr>";
+          $count = $count + 1;
+        }
+      }
+    }
+
+   ?>
+
+
+
+</table>
 
 
  </form>
--- phpproj/phpproj/reports.php
+++ phpproj/phpproj/reports.php
@@ -2,6 +2,49 @@
   include_once 'header.php';
 ?>
 
+<?php
+
+/*
+
+  $uid = $_SESSION["u_id"];
+
+  $sql = "SELECT * FROM assessments WHERE report_owner='$uid'";
+  $result = mysqli_query($conn, $sql);
+  $resultCheck = mysqli_num_rows($result);
+
+  $count = 0;
+
+  if($resultCheck > 0) {
+    while (($row = mysqli_fetch_assoc($result)) && ($count < 10)) {
+      if ($row['active'] > 0) {
+        echo "<tr>";
+        echo "<th>" . $row['report_name'] . "</th>";
+
+        $score = 0;
+        $question1 = $row['question_1'];
+        $question2 = $row['question_2'];
+        $question3 = $row['question_3'];
+        $question4 = $row['question_4'];
+        $question5 = $row['question_5'];
+        $question6 = $row['question_6'];
+        $question7 = $row['question_7'];
+        $question8 = $row['question_8'];
+        $question9 = $row['question_9'];
+        $question10 = $row['question_10'];
+
+
+
+        echo "<th> 100 </th>";
+        echo "<th>" . $row['dt_created'] . "</th>";
+        echo "<th> Delete </th>"; //this need to be a link that will send a request to move 'active' to 0
+
+        echo "</tr>";
+        $count = $count + 1;
+      }
+    }
+  }
+*/
+
 <script type="text/javascript">
 window.onload = function () {
   var chart = new CanvasJS.Chart("chartContainer",
